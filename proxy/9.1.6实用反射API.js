a27b94fd8a8001f37b86611c865ce4ddac3bee64
paulChess <sjq15251852316@163.com>
2021-03-04 01:05:12 +0800

    实用反射api学习

--- proxy/9.1.6实用反射API.js
+++ proxy/9.1.6实用反射API.js
@@ -1,3 +1,35 @@
-// 优先实用反射API
-// 反射API与对象API
-// 状态标记
+// 某些场景下优先实用反射API
+/**
+ * (1) 反射API并不限于捕获处理程序
+ * (2) 大多数反射API方法在Object类型上有对应的方法
+ */
+
+ // Object上的方法适用于通用程序， 而反射方法适用于细粒度的对象控制与操作
+
+ // 状态标记: 很多反射方法返回的布尔值，表示意图执行的操作是否成功
+
+ // 初始代码
+ const o = {};
+ try {
+   Object.defineProperty(o, 'foo', 'bar');
+   console.log('success');
+ } catch(e) {
+   console.log('fail');
+ }
+
+ // 改造代码
+ const o = {};
+ if (Reflect.defineProperty(o, 'foo', {value: 'bar'})) {
+   console.log('success');
+ } else {
+   console.log('fail');
+ }
+
+ // 提供状态标记的方法
+ /**
+  * 1. Reflect.defineProperty()
+  * 2. Reflect.preventExtensions()
+  * 3. Reflect.setPropertyOf()
+  * 4. Reflect.set()
+  * 5. Reflect.deleteProperty()
+  */
