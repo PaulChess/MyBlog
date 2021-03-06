e1a7e92bb865d1573faa24ff59f31c297f93657b
paulChess <sjq15251852316@163.com>
2021-03-06 22:58:07 +0800

    代理捕获器get|set

---
+++ proxy/9.2代理捕获器与反射API/9.2.1get.js
@@ -0,0 +1,34 @@
+// get()捕获器在获取属性值的操作中被调用
+// 对应的反射APi方法为Reflect.get()
+
+
+/**
+ * get方法拦截的操作
+ * 1. proxy.property
+ * 2. proxy[property]
+ * 3. Object.create(proxy)[property]
+ * 4. Reflect.get(proxy, property, receiver)
+ */
+
+const myTarget = {};
+
+const proxy = new Proxy(myTarget, {
+  /**
+   * @param {*} target 目标对象
+   * @param {*} property 属性
+   * @param {*} receiver 代理对象或继承代理对象的对象
+   * 返回值无限制
+   */
+  get(target, property, receiver) {
+    console.log('get()');
+    return Reflect.get(...arguments);
+  }
+})
+
+proxy.foo;
+
+/**
+ * 注: 捕获器不变式
+ * 1. 如果target.property不可写且不可配置，则处理程序返回的值必须与target.property匹配
+ * 2. 如果target.property不可配置且[[Get]]特性为undefined, 则处理程序返回的值也必须是undefined
+ */
---
+++ proxy/9.2代理捕获器与反射API/9.2.2set.js
@@ -0,0 +1,32 @@
+// set捕获器在设置值时会调用，对应反射API为Reflect.set()
+
+/**
+ * 拦截操作:
+ * 1. proxy.property = value
+ * 2. proxy[property] = value
+ * 3. Object.create(proxy)[property] = value
+ * 4. Reflect.set(proxy, property, value, receiver)
+ */
+const myTarget = {};
+
+const proxy = new Proxy(myTarget, {
+  /**
+   * @param {*} target 目标对象
+   * @param {*} property 属性
+   * @param {*} value
+   * @param {*} receiver 接收最初赋值的对象
+   * 返回值: true表示成功; false表示失败， 严格模式下抛出TypeError
+   */
+  set(target, property, value, receiver) {
+    console.log('set()');
+    return Reflect.get(...arguments);
+  }
+})
+
+proxy.foo = 'bar';
+
+/**
+ * 捕获器不变式
+ * 1. 如果target.property是不可写且不可配置，则不能修改目标属性的值
+ * 2. 如果target.property不可配置且[[Set]]特性为undefined, 则不能修改目标属性的值。在严格模式下，处理程序中返回false会抛出TypeError
+ */
